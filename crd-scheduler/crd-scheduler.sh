cf2ba9efb17364b944425ef8ff815f5762b6fa09
wsszh <492558361@qq.com>
2019-01-08 20:50:19 +0800

    update crd-scheduler.sh

--- crd-scheduler/crd-scheduler.sh
+++ crd-scheduler/crd-scheduler.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 SERVER='localhost:8001'
-CRDTYPE='ncapps'
+CRD_PLURAL='ncapps'
+CRD_SINGULAR='ncapp'
 while true;
 do
-    for CRDNAME in $(kubectl --server $SERVER get $CRDTYPE -o json | jq '.items[] | select(.spec.nodeName == null) | .metadata.name' | tr -d '"')
+    for CRDNAME in $(kubectl --server $SERVER get $CRD_PLURAL -o json | jq '.items[] | select(.spec.nodeName == null) | .metadata.name' | tr -d '"')
     do
         echo $CRDNAME
         NODES=($(kubectl --server $SERVER get nodes -o json | jq '.items[].metadata.name' | tr -d '"'))        
@@ -16,7 +17,7 @@ do
             CHOSEN=${NODES[$[ $RANDOM % $NUMNODES ]]}
             echo $CHOSEN
         done
-        kubectl patch myapp $CRDNAME --type merge --patch $'spec:\n nodeName: '$CHOSEN''
+        kubectl patch $CRD_SINGULAR $CRDNAME --type merge --patch $'spec:\n nodeName: '$CHOSEN''
     done
 
     sleep 1
